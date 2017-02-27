1d69aaad12965a076ac2dbbda15569c2b8a55ca1
Alexander Obukhov <github@sprql.space>
2017-02-27 21:28:27 +0300

    Add slim template engine

--- config/dev.exs
+++ config/dev.exs
@@ -35,7 +35,7 @@ config :planner, Planner.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|slim|slime)$}
     ]
   ]
 
